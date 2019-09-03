94001e91cde3d2842e33a94bf47d75e80f706a94
VanesNery <nery_van@hotmail.com>
2019-09-02 22:45:35 -0300

    ajustando css, inserindo funçao ao js e atualizacao no html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 document.getElementById("btn_tweet").addEventListener("click", botaotweet);
 
 function botaotweet (){
-    
+  let Guardar = document.getElementById("Texto").value;
+  let Texto = localStorage.setItem("key", Guardar);
+  
+document.getElementById("mensagem").innerHTML+= localStorage.getItem("key");
 }
