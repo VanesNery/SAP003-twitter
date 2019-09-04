ec1cbbc3b0dcf5d51215b71406d6c9219b42f47f
VanesNery <nery_van@hotmail.com>
2019-09-04 02:48:44 -0300

    inserindo contador e colocando cor nele

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
-// $("Texto").keydown(function botaotweet (){  
-//   let msg = [];
-//   let Guardar = document.getElementById("Texto").value;
-//   let Texto = localStorage.setItem("key", Guardar);
-//   document.getElementById("btn_tweet").disabled = true;
-//   msg.push(localStorage.getItem("key"));
-//   document.getElementById("mensagem").innerHTML+= msg; 
-// });
-document.getElementById("btn_tweet").addEventListener("click", botaotweet);
-function botaotweet (){
-  let msg = [];
-  let Guardar = document.getElementById("Texto").value;
-  let Texto = localStorage.setItem("key", Guardar);
-  msg.push(localStorage.getItem("key"));
-  document.getElementById("mensagem").innerHTML+= msg;
+const texto = document.getElementById("Texto").addEventListener("keyup", contaChar);
+function contaChar(){
+let max = 140;
+let texto = document.getElementById("Texto").value.length;
+let res = max - texto;
+document.getElementById("contador").innerHTML = res;
+
+if(texto === "0"){
+    document.getElementById("btn_tweet").disabled = false;
 }
+
+else{
+    document.getElementById("btn_tweet").disabled = true;
+    return botaotweet;
+}
+}
+
+document.getElementById("btn_tweet").addEventListener("click", botaotweet);
+    function botaotweet (){
+      let msg = [];
+      let Guardar = document.getElementById("Texto").value;
+      let Texto = localStorage.setItem("key", Guardar);
+      msg.push(localStorage.getItem("key"));
+      document.getElementById("mensagem").innerHTML+= msg;
+    }
