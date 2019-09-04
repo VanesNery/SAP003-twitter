26f90e97a28d3aae68bd95ae94ede2d4568e9efe
VanesNery <nery_van@hotmail.com>
2019-09-04 00:02:06 -0300

    atualizaçao

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+// $("Texto").keydown(function botaotweet (){  
+//   let msg = [];
+//   let Guardar = document.getElementById("Texto").value;
+//   let Texto = localStorage.setItem("key", Guardar);
+//   document.getElementById("btn_tweet").disabled = true;
+//   msg.push(localStorage.getItem("key"));
+//   document.getElementById("mensagem").innerHTML+= msg; 
+// });
 document.getElementById("btn_tweet").addEventListener("click", botaotweet);
-
 function botaotweet (){
+  let msg = [];
   let Guardar = document.getElementById("Texto").value;
   let Texto = localStorage.setItem("key", Guardar);
-  
-document.getElementById("mensagem").innerHTML+= localStorage.getItem("key");
+  msg.push(localStorage.getItem("key"));
+  document.getElementById("mensagem").innerHTML+= msg;
 }
