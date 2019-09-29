98023ad3a0456911211ce59ad4b7ec104b257d5d
vanesnery <nery_van@hotmail.com>
2019-09-29 14:45:50 -0300

    versao 0.0.2 e 0.0.3 ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 let message = document.getElementById("message");
 let btntweet = document.getElementById("btntweet");
 let print = document.getElementById("print");
+const MAX = 140;
 
 btntweet.addEventListener("click", buttontweet);
 
 function buttontweet (e){
-  let prinText = document.createElement("li");
+  let prinText = document.createElement("div");
   prinText.setAttribute("id", "printE")
   prinText.textContent = message.value;
   print.appendChild(prinText);
   message.value = "";
 }
+
+message.addEventListener("keyup", contChar);
+
+function contChar(e){
+  let res = MAX - message.value.length;
+  document.getElementById("counter").innerHTML = res;
+  
+  if(message === 0 || message > 140){
+    document.getElementById("counter").style.color = "#f01c1c";
+  }
+  else if (message <= 120){
+    btntweet.removeAttribute("disabled");
+    document.getElementById("btntweet").addEventListener("click", buttontweet);
+   }
+  else{
+    btntweet.removeAttribute("disabled");
+    document.getElementById("counter").style.color = "#32CD32";
+    document.getElementById("btntweet").addEventListener("click", buttontweet);
+  }
+  }
